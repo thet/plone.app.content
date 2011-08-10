81657b7e1ee1d177c31520d7c76d19e6c115d6cb
nobody <nobody@localhost>
2011-08-10 12:41:55 +0000

    The factories menu don't use this view anymore
    svn path=/plone.app.content/branches/encolpe-default-view/; revision=51583

--- plone/app/content/browser/folderfactories.py
+++ plone/app/content/browser/folderfactories.py
@@ -38,11 +38,10 @@ class FolderFactoriesView(BrowserView):
     @memoize
     def add_context(self):
         context_state = getMultiAdapter((self.context, self.request), name='plone_context_state')
-        return context_state.folder()
-
-    # NOTE: This is also used by plone.app.contentmenu.menu.FactoriesMenu.
-    # The return value is somewhat dictated by the menu infrastructure, so
-    # be careful if you change it
+        if context_state.is_structural_folder():
+            return self.context
+        else:
+            return context_state.folder()
 
     def addable_types(self, include=None):
         """Return menu item entries in a TAL-friendly form.
